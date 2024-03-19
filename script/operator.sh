28468251d8bbc59e878173ed92748f131168aed0
RogerJung <engineer54rj@gmail.com>
2024-03-19 15:43:19 +0800

    Update scripts.

--- script/operator.sh
+++ script/operator.sh
@@ -3,5 +3,10 @@
 source ./install/setup.bash
 source ./external/autoware_manual_control/install/setup.bash
 
-RUST_LOG=error ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -e tcp/10.10.0.71:8001 &
-ros2 run autoware_manual_control keyboard_control
+VEHICLE_IP=10.10.0.71
+OPERATOR_IP=10.10.0.72
+
+# Control
+RUST_LOG=error ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -e tcp/${VEHICLE_IP}:8001 &
+
+sleep 2 && ros2 run autoware_manual_control keyboard_control
--- script/vehicle.sh
+++ script/vehicle.sh
@@ -2,5 +2,8 @@
 
 source ./install/setup.bash
 
+VEHICLE_IP=10.10.0.71
+OPERATOR_IP=10.10.0.72
+
 # Control
-ros2 run remote_control remote_control -l tcp/10.10.0.71:8001
+ros2 run remote_control remote_control -l tcp/${VEHICLE_IP}:8001
