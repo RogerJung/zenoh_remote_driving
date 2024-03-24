80dacbaea520ba424a7d5463d256c0725b1aa1fc
RogerJung <engineer54rj@gmail.com>
2024-03-24 20:31:48 +0800

    Update scripts.

--- script/operator.sh
+++ script/operator.sh
@@ -6,7 +6,17 @@ source ./external/autoware_manual_control/install/setup.bash
 VEHICLE_IP=10.10.0.71
 OPERATOR_IP=10.10.0.72
 
-# Control
-RUST_LOG=error ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -e tcp/${VEHICLE_IP}:8001 &
+# Init. tmux
+tmux kill-server
 
+# Autoware & Lidar
+export ROS_DOMAIN_ID=1
+tmux new -s bridge_autoware -d
+tmux send-keys -t bridge_autoware "RUST_LOG=info ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -e tcp/${VEHICLE_IP}:8001" ENTER
+ros2 run remote_lidar remote_lidar
+
+# Control
+export ROS_DOMAIN_ID=2
+tmux new -s bridge_control -d
+tmux send-keys -t bridge_control "RUST_LOG=info ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -e tcp/${VEHICLE_IP}:8002" ENTER
 sleep 2 && ros2 run autoware_manual_control keyboard_control
--- script/vehicle.sh
+++ script/vehicle.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+source ${HOME}/repos/F1EIGHT/install/setup.bash
 source ./install/setup.bash
+source /opt/ros/humble/setup.bash
 
 VEHICLE_IP=10.10.0.71
 OPERATOR_IP=10.10.0.72
 
+# Init. tmux
+tmux kill-server
+
+# Autoware
+export ROS_DOMAIN_ID=1
+tmux new -s bridge_autoware -d
+tmux send-keys -t bridge_autoware "RUST_LOG=info ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -l tcp/${VEHICLE_IP}:8001" ENTER
+tmux new -s autoware -d
+tmux send-keys -t autoware "ros2 launch autoware_launch autoware.launch.xml map_path:=${HOME}/autoware_map/sample-map-planning vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit" ENTER
+
+while true
+do
+    ros2 bag play rosbag2_2024_03_24-17_07_09/
+done
+
 # Control
-ros2 run remote_control remote_control -l tcp/${VEHICLE_IP}:8001
+export ROS_DOMAIN_ID=2
+ros2 run remote_control remote_control -l tcp/${VEHICLE_IP}:8002
