42d3a0c23b3b6643bdb9b4e8c0b6577cc4b61d00
RogerJung <engineer54rj@gmail.com>
2024-04-26 21:10:50 +0800

    Add ffmpeg script.

---
+++ script/ffmpeg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VEHICLE_IP=10.10.0.71
+
+while true; do
+
+    ffmpeg -i /dev/video0 -vf scale=1920:1080 -tune zero_latency -f mpegts tcp://${VEHICLE_IP}:8001?connect
+
+    # Check whether "ffmpeg" execute success or not.
+    if [ $? -eq 0 ]; then
+        echo "FFmpeg success..."
+        break
+    else
+        echo "FFmpeg error, restarting..."
+        sleep 1  # Retry
+    fi
+done
