4f3ee1f66fc3c6ad3c2bb4f37dbbec9df918b6c8
RogerJung <engineer54rj@gmail.com>
2024-04-27 16:20:15 +0800

    Update timestamp scripts.

--- src/timestamp/recv.py
+++ src/timestamp/recv.py
@@ -5,30 +5,25 @@ SEND_IP='127.0.0.1'
 PORT=12345
 
 def start_server(host, port):
-    # 建立 socket
+    # Create socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # 綁定到指定的 IP 位址和通訊埠號
         s.bind((host, port))
-        # 最多允許一個連線
         s.listen(1)
-        print(f"伺服器正在監聽 {host}:{port}")
+        print(f"Server listening at {host}:{port}")
 
-        # 等待連線
+        # Waiting connection
         conn, addr = s.accept()
-        print(f"已建立連線：{addr}")
+        print(f"Establish connection: {addr}")
         while conn:
-            # 接收來自客戶端的資料
+            # Recv client's date
             data = conn.recv(1024)
             if data:
                 timestamp = data.decode()
-                # print(f"接收到的 timestamp: {timestamp}")
+
                 f_time = float(timestamp)
                 c_time = time.time()
                 latency = c_time - f_time
                 print(f"latency : {latency} sec")
                 
-            else:
-                print("沒有收到資料")
-
 if __name__ == "__main__":
     start_server(SEND_IP, PORT)
