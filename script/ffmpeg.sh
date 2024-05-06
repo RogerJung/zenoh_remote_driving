076a02dfa8521663d337bbaefd7275c41fab6e97
RogerJung <engineer54rj@gmail.com>
2024-05-06 22:57:25 +0800

    Update ffmpeg script.

--- script/ffmpeg.sh
+++ script/ffmpeg.sh
@@ -4,8 +4,7 @@ VEHICLE_IP=10.10.0.71
 
 while true; do
 
-    ffmpeg -hwaccel cuda -f v4l2 -input_format mjpeg -framerate 30 -video_size 1920x1080 -i /dev/video0 -tune zero_latency -f mpegts tcp://${VEHICLE_IP}:8003?connect
-    # ffmpeg -i /dev/video0 -vf scale=1920:1080 -tune zero_latency -f mpegts tcp://${VEHICLE_IP}:8003?connect
+    ffmpeg -hwaccel cuda -f v4l2 -input_format mjpeg -framerate 30 -video_size 1920x1080 -i /dev/video0 -tune zero_latency -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}':x=10:y=10:fontsize=24:fontcolor=black" -f mpegts tcp://${VEHICLE_IP}:8003?connect
 
     # Check whether "ffmpeg" execute success or not.
     if [ $? -eq 0 ]; then
