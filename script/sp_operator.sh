38dc34a36217c7adfcb27e3621f109cff249439d
RogerJung <engineer54rj@gmail.com>
2024-05-09 00:39:19 +0800

    Update script folder.

--- script/sp_operator.sh
+++ script/sp_operator.sh
@@ -16,9 +16,9 @@ tmux new -s bridge_ffplay -d
 tmux send-keys -t bridge_ffplay "bash ./script/ffplay.sh" ENTER
 
 # Autoware & Lidar
+export ROS_DOMAIN_ID=1
 tmux new -s bridge_autoware -d
 tmux send-keys -t bridge_autoware "RUST_LOG=info ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -e tcp/${VEHICLE_IP}:8001" ENTER
 ros2 run remote_lidar remote_lidar &
 
-# Control
-sleep 2 && ros2 run g923_control g923_control
+
