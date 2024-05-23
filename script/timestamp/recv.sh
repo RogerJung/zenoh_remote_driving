d07bac092bf33dc44f8c111101dc801c7cfb8133
RogerJung <engineer54rj@gmail.com>
2024-05-23 15:22:38 +0800

    Update font size.

--- script/timestamp/recv.sh
+++ script/timestamp/recv.sh
@@ -3,5 +3,5 @@
 
 ffmpeg -fflags nobuffer -flags low_delay \
     -f mpegts -i tcp://${VEHICLE_IP}:8080?connect \
-    -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}.%{eif\:(1000 * (t - floor(t)))\:d}': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: x=w-tw-10: y=10" \
+    -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}.%{eif\:(1000 * (t - floor(t)))\:d}': fontcolor=white: fontsize=38: box=1: boxcolor=black@0.5: x=w-tw-10: y=10" \
     output.mp4
--- script/timestamp/send.sh
+++ script/timestamp/send.sh
@@ -5,7 +5,7 @@ while true; do
     ffmpeg -f v4l2 -input_format mjpeg -framerate 30 \
         -video_size 1920x1080 \
         -i /dev/video0 -tune zero_latency \
-        -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}.%{eif\:(1000 * (t - floor(t)))\:d}':x=10:y=10:fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5" \
+        -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}.%{eif\:(1000 * (t - floor(t)))\:d}':x=10:y=10:fontcolor=white: fontsize=38: box=1: boxcolor=black@0.5" \
         -f mpegts tcp://${VEHICLE_IP}:8003?connect
 
     # Check whether "ffmpeg" execute success or not.
