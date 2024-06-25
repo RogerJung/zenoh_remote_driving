b8d120a5d4115dd52d7687a10b36b803f3ac97bd
RogerJung <engineer54rj@gmail.com>
2024-06-25 21:06:30 +0800

    Update ffmpeg parameter.

--- script/ffmpeg.sh
+++ script/ffmpeg.sh
@@ -2,7 +2,7 @@
 
 while true; do
 
-    ffmpeg -hwaccel cuda -f v4l2 -input_format mjpeg -framerate 30 -video_size 1920x1080 -i /dev/video0 -tune zero_latency -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}.%{eif\:(1000 * (t - floor(t)))\:d}':x=10:y=10:fontsize=24:fontcolor=black" -f mpegts tcp://${VEHICLE_IP}:8003?connect
+    ffmpeg -f v4l2 -input_format mjpeg -framerate 30 -video_size 1920x1080 -i /dev/video0 -tune zero_latency -vf "drawtext=text='%{localtime\:%Y-%m-%d %H\\\\\:%M\\\\\:%S}.%{eif\:(1000 * (t - floor(t)))\:d}':x=10:y=10:fontsize=24:fontcolor=black" -f mpegts tcp://${VEHICLE_IP}:8003?connect
 
     # Check whether "ffmpeg" execute success or not.
     if [ $? -eq 0 ]; then
--- script/ffplay.sh
+++ script/ffplay.sh
@@ -2,7 +2,7 @@
 
 PORT=8080
 
-ffplay_command="ffplay -fflags nobuffer -flags low_delay -f mpegts tcp://${VEHICLE_IP}:${PORT}?connect"
+ffplay_command="ffplay -fflags nobuffer -flags low_delay -framedrop -f mpegts tcp://${VEHICLE_IP}:${PORT}?connect"
 
 while :
 do
